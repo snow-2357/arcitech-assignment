ad6cd1cab77aee39c06fb333bf033c3c9d6dba16
simantaray <simantaray89@gmail.com>
2024-02-23 20:11:47 +0530

    pie chart

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@
 import "./App.css";
 import BarChartDisplay from "./components/BarChart";
 import LineChartDisplay from "./components/LineChart";
+import PieChartDisplay from "./components/PieChart";
 
 import { apiData } from "./data";
 function App() {
@@ -10,6 +11,7 @@ function App() {
     <div>
       <BarChartDisplay data={apiData.category_distribution} />
       <LineChartDisplay data={apiData.response_times.week_wise} />
+      <PieChartDisplay data={apiData.user_satisfaction.ratings} />
     </div>
   );
 }
